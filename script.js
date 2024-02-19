e10f6a8ef710e2692263fa51fec115f581804805
Janardhan Biharilal Yadav <63313078+janardhanyadav@users.noreply.github.com>
2024-02-19 20:50:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+    let inputs = document.querySelector("input")
+	let first_name = inputs[0].value
+	let last_name = inputs[1].value
 
+	let full_name = first_name + " " + last_name
+	alert(full_name)
 }
